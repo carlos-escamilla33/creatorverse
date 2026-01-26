c9548063542ea2d3350626e1fdd639660ebe52f4
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-26 15:13:51 -0800

    created supabase client

--- src/client.js
+++ src/client.js
@@ -1,3 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const URL = "https://vrxezctlgfhyhtevoroh.supabase.co"
-const API_KEY = "sb_publishable_zOgq3tIJvrhM1Nysq_fh2Q_YZryezL-";
+
+const URL = import.meta.env.URL;
+const API_KEY = import.meta.env.API_KEY;
+export const supabase = createClient(URL, API_KEY);
