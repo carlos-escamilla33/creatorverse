2a0c3cd75931d9a62d14c08d96826ec9494a4866
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-27 15:36:56 -0800

    Added placeholder data for pages

---
+++ src/pages/AddCreator.jsx
@@ -0,0 +1,8 @@
+
+const AddCreator = () => {
+    return (
+        <h1>Add Creator Page</h1>
+    )
+}
+
+export default AddCreator;
---
+++ src/pages/EditCreator.jsx
@@ -0,0 +1,8 @@
+
+const EditCreator = () => {
+    return (
+        <h1>Edit Creator Page</h1>
+    )
+}
+
+export default EditCreator;
