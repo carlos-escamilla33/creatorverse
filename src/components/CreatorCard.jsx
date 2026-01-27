2c9074548d6e0c4d67234143b07c347704e3d362
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-26 23:54:51 -0800

    rendering youtube channel image

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -1,10 +1,21 @@
-const CreatorCard = ({creator}) => {
-
+const CreatorCard = ({ creator }) => {
+  const username = creator.url.match(/@([^/]+)/)?.[1];
   return (
     <article className="creator-card">
+      <header className="creator-avatar">
+        <img
+          src={`https://unavatar.io/youtube/${username}`}
+          alt={`${creator.name} avatar`}
+          onError={(e) => {
+            e.target.src = "https://via.placeholder.com/150?text=No+Image";
+          }}
+        />
+      </header>
       <header>{creator.name}</header>
       {creator.description}
-      <footer><a href={creator.url}>Visit Channel →</a></footer>
+      <footer>
+        <a href={creator.url}>Visit Channel →</a>
+      </footer>
     </article>
   );
 };
