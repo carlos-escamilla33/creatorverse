04bc062daaed5b6f62114f9cd872f2a919507d68
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-28 15:43:20 -0800

    completed adding inputs for EditCreator page

--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -1,14 +1,15 @@
 
 const AddCreator = () => {
     return (
-        <section className="container">
+        <form className="container">
             <h1>Add Creator</h1>
             <input type="text" name="name" placeholder="Creator Name" aria-label="Text" />
             <input type="text" name="Youtube url" placeholder="Youtube URL" aria-label="Text"/>
             <input type="text" name="Twitter url" placeholder="Twitter URL" aria-label="Text"/>
             <input type="text" name="Instagram url" placeholder="Instagram URL" aria-label="Text"/>
             <textarea name="description" placeholder="Write a short description of your creator..." aria-label="Short description"/>
-        </section>
+            <input type="submit"/>
+        </form>
     )
 }
 
--- src/pages/EditCreator.jsx
+++ src/pages/EditCreator.jsx
@@ -1,7 +1,15 @@
 
 const EditCreator = () => {
     return (
-        <h1>Edit Creator Page</h1>
+           <form className="container">
+            <h1>Edit Creator</h1>
+            <input type="text" name="name" placeholder="Creator Name" aria-label="Text" />
+            <input type="text" name="Youtube url" placeholder="Youtube URL" aria-label="Text"/>
+            <input type="text" name="Twitter url" placeholder="Twitter URL" aria-label="Text"/>
+            <input type="text" name="Instagram url" placeholder="Instagram URL" aria-label="Text"/>
+            <textarea name="description" placeholder="Write a short description of your creator..." aria-label="Short description"/>
+            <input type="submit"/>
+        </form>
     )
 }
 
