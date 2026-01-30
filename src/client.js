cd92724ae38805a34c94e0efe11e1e465cdad8e9
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-01-30 14:05:45 -0800

    added enironment variables

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = import.meta.env.URL;
-const API_KEY = import.meta.env.API_KEY;
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 export const supabase = createClient(URL, API_KEY);
--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,43 +1,45 @@
 import CreatorCard from "../components/CreatorCard";
+import { useCreator } from "../hooks/useCreators";
 
-const testCreators = [
-  {
-    id: 1,
-    name: "Mr Beast",
-    url: "https://www.youtube.com/@MrBeast",
-    description: "He is super rich",
-  },
-  {
-    id: 2,
-    name: "Neetcode",
-    url: "https://www.youtube.com/@NeetCode",
-    description: "Preparing for coding interviews? Checkout neetcode.io",
-  },
-  {
-    id: 3,
-    name: "Sam Sulek",
-    url: "https://www.youtube.com/@sam_sulek",
-    description: "Bodybuilding Content",
-  },
-  {
-    id: 4,
-    name: "Miami Heat",
-    url: "https://www.youtube.com/@MiamiHEAT",
-    description: "🏆🏆🏆 #HEATNation #HEATCulture",
-  },
-  {
-    id: 5,
-    name: "Mr Beast",
-    url: "https://www.youtube.com/@MindTheGamePodcast",
-    description:
-      "Mind The Game is an entry into the heralded basketball minds of LeBron James and Steve Nash.",
-  },
-];
+// const testCreators = [
+//   {
+//     id: 1,
+//     name: "Mr Beast",
+//     url: "https://www.youtube.com/@MrBeast",
+//     description: "He is super rich",
+//   },
+//   {
+//     id: 2,
+//     name: "Neetcode",
+//     url: "https://www.youtube.com/@NeetCode",
+//     description: "Preparing for coding interviews? Checkout neetcode.io",
+//   },
+//   {
+//     id: 3,
+//     name: "Sam Sulek",
+//     url: "https://www.youtube.com/@sam_sulek",
+//     description: "Bodybuilding Content",
+//   },
+//   {
+//     id: 4,
+//     name: "Miami Heat",
+//     url: "https://www.youtube.com/@MiamiHEAT",
+//     description: "🏆🏆🏆 #HEATNation #HEATCulture",
+//   },
+//   {
+//     id: 5,
+//     name: "Mr Beast",
+//     url: "https://www.youtube.com/@MindTheGamePodcast",
+//     description:
+//       "Mind The Game is an entry into the heralded basketball minds of LeBron James and Steve Nash.",
+//   },
+// ];
 
 const ShowCreators = () => {
+    const {creators} = useCreator();
   return (
     <section className="content-card-container">
-      {testCreators.map(testCreator => {
+      {creators.length == 0 ? <p>No Data Available...</p>:creators.map(testCreator => {
         return (
             <CreatorCard key={testCreator.id} creator={testCreator} />
         )
